6441a2b8dd403c4dd19f2b105cd183699237532e
stevenstevanus <steven.stevanus@indosystem.com>
2017-09-19 15:14:25 +0700

    test error 15

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.send('CI/CD with CircleCI!')
-})
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
