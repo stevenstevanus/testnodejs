3729b34870ef4592e7835c75a3af4cc220170012
stevenstevanus <steven.stevanus@indosystem.com>
2017-09-22 11:05:56 +0700

    test deploy 57

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('CI/CD with CircleCI!')
+  res.send('CI/CD with CircleCI!!')
 })
 
 app.listen(3000, function () {
